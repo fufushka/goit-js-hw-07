580de6f281af622b230344bcc9ceef5b762b2e13
fufushka <dimaukropdred@gmail.com>
2023-05-14 13:05:49 +0300

    added homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const markup = galleryItems
+  .map((item) => {
+    return `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}" /></a>
+      </li>
+    `;
+  })
+  .join("");
+refs.gallery.innerHTML = markup;
+
+refs.gallery.addEventListener("click", openModalWindow);
+function openModalWindow(event) {
+  event.preventDefault();
+
+  const imageUrl = event.target.dataset.source;
+  console.log(imageUrl);
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+`);
+  instance.show();
+
+  const visible = instance.visible();
+
+  document.addEventListener("keydown", closeModalOnEscape);
+  function closeModalOnEscape(event) {
+    if (event.code === "Escape" && visible) {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map((item) => {
+    return `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>
+      </li>
+    `;
+  })
+  .join("");
+refs.gallery.innerHTML = markup;
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
